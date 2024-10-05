9c9962c247fdf3333c99e4a516066ebe3bb80c14
Ketan Gupta <ckg4155@gmail.com>
2024-10-05 12:46:34 +0530

    Download page for admin panle UI ready

--- resources/views/Admin/user_download_page.blade.php
+++ resources/views/Admin/user_download_page.blade.php
@@ -6,7 +6,7 @@
 
             <div class="page-title-right">
                 <ol class="breadcrumb m-0">
-                    <li class="breadcrumb-item"><a href="{{url('/admin-dashboard')}}"><i class="ri-home-5-fill"></i></a></li>
+                    <li class="breadcrumb-item"><a href="{{ url('/admin-dashboard') }}"><i class="ri-home-5-fill"></i></a></li>
                     <li class="breadcrumb-item pg-title active"></li>
                 </ol>
             </div>
@@ -15,3 +15,66 @@
     </div>
 </div>
 <!-- end page title -->
+<div class="card">
+    <div class="card-header">
+        <!-- Button trigger modal -->
+        <button type="button" class="btn btn-primary" data-bs-toggle="modal" data-bs-target="#staticBackdrop">
+            Add file
+        </button>
+    </div>
+    <div class="card-body">
+        <div class="table-responsive">
+            <table class="table" id="fileTable">
+                <thead>
+                    <tr>
+                        <th>S.no.</th>
+                        <th>Title</th>
+                        <th>Subtitle</th>
+                        <th>Actions</th>
+                    </tr>
+                </thead>
+                <tbody></tbody>
+                <tfoot>
+                    <tr>
+                        <td colspan="4" style="text-align: center!important; font-style:italic;" class="text-muted">The files will be visible in all user panel/Download Page, for all active user</td>
+                    </tr>
+                </tfoot>
+            </table>
+        </div>
+    </div>
+</div>
+
+<!-- Modal -->
+<div class="modal fade" id="staticBackdrop" data-bs-backdrop="static" data-bs-keyboard="false" tabindex="-1" aria-labelledby="staticBackdropLabel"
+     aria-hidden="true">
+    <div class="modal-dialog modal-dialog-centered">
+        <div class="modal-content">
+            <div class="modal-header">
+                <h1 class="modal-title fs-5" id="staticBackdropLabel">Modal title</h1>
+                <button type="button" class="btn-close" data-bs-dismiss="modal" aria-label="Close"></button>
+            </div>
+            <div class="modal-body">
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="file_title" name="file_title" placeholder="File title">
+                    <label for="file_title">Title</label>
+                </div>
+                <div class="form-floating mb-3">
+                    <input type="text" class="form-control" id="file_subtitle" name="file_subtitle" placeholder="File subtitle">
+                    <label for="file_subtitle">Subtitle</label>
+                </div>
+                <div class="form-group">
+                    <input type="file" class="form-control" id="file" name="file" placeholder="file">
+                </div>
+            </div>
+            <div class="modal-footer">
+                <button type="button" class="btn btn-primary">Add</button>
+                <button type="button" class="btn btn-secondary" data-bs-dismiss="modal">Close</button>
+            </div>
+        </div>
+    </div>
+</div>
+<script>
+    $(document).ready(function(){
+        $('#fileTable').dataTable();
+    });
+</script>
